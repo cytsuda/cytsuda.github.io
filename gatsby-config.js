f4d14f9367f86c08957d772899b3c9c85b0ea28b
Carlos Yoshio Tsuda <cyt231@gmail.com>
2021-05-19 17:28:12 -0300

    Erro no build netlify

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,7 +27,7 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `markdown-pages`,
-        path: `${__dirname}/src/data`
+        path: `./src/data`
       }
     }
   ],
